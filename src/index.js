e140508dcd8ba4a74bf4dfc26f09e502c681a02c
EgorMax <egormaks2004@gmail.com>
2020-10-26 00:11:39 +0400

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  if (n < 0) {
+    n = -n
+    return (n.toString().split('').reverse().join('') * Math.sign(n));
+  } else {
+    return (n.toString().split('').reverse().join('') * Math.sign(n));
+  }
 }
